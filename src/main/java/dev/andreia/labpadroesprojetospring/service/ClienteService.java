1e74983e0a00aae0c6e46ecb6d531d17aae99920
Andreia-Nunes <andreia.nunes@academico.ifpb.edu.br>
2023-09-23 19:37:35 -0300

    Criação das interfaces ClienteService e ViaCepService

---
+++ src/main/java/dev/andreia/labpadroesprojetospring/service/ClienteService.java
@@ -0,0 +1,16 @@
+package dev.andreia.labpadroesprojetospring.service;
+
+import dev.andreia.labpadroesprojetospring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+}
